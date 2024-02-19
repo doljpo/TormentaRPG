4d1b91373f3f3104fe392f8c4d8081e68e694c85
David Oliveira <dol.jpo@hotmail.com>
2024-02-19 19:51:47 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,149 @@
+function filtrarItens() {
+    var categoriaSelecionada = document.getElementById('categoria').value;
+    var subcategoriaSelecionada = document.getElementById('subcategoria').value;
+    var equipamentosFiltrados = equipamentos.filter(function (item) {
+        return item.categoria === categoriaSelecionada && item.subcategoria === subcategoriaSelecionada;
+    });
+    exibirItens(equipamentosFiltrados);
+}
+
+function atualizarSubcategorias() {
+    var categoriaSelecionada = document.getElementById('categoria').value;
+    var subcategoriaSelect = document.getElementById('subcategoria');
+    subcategoriaSelect.innerHTML = '<option value="">Selecione uma subcategoria</option>';
+
+    if (categoriaSelecionada === 'Armas') {
+        adicionarOpcoesSubcategoria(['Espada', 'Machado']);
+    } else if (categoriaSelecionada === 'Armaduras') {
+        adicionarOpcoesSubcategoria(['Leve', 'Pesada']);
+    } else if (categoriaSelecionada === 'Escudos') {
+        adicionarOpcoesSubcategoria(['Pequeno', 'Médio']);
+    } else if (categoriaSelecionada === 'Itens') {
+        adicionarOpcoesSubcategoria(['Comida', 'Geral']);
+    } else if (categoriaSelecionada === 'Poções') {
+        adicionarOpcoesSubcategoria(['Vida', 'Mana']);
+    } else {
+        subcategoriaSelect.style.display = 'none';
+    }
+}
+
+function adicionarOpcoesSubcategoria(subcategorias) {
+    var subcategoriaSelect = document.getElementById('subcategoria');
+    subcategorias.forEach(function (subcategoria) {
+        var option = document.createElement('option');
+        option.textContent = subcategoria;
+        option.value = subcategoria;
+        subcategoriaSelect.appendChild(option);
+    });
+    subcategoriaSelect.style.display = 'inline';
+}
+
+function exibirItens(itens) {
+    var container = document.getElementById('equipamentos-container');
+    container.innerHTML = ''; // Limpa o conteúdo anterior
+
+    itens.forEach(function (item) {
+        var card = document.createElement('div');
+        card.classList.add('card');
+        var cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        var cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = item.nome;
+        var tipo = document.createElement('p');
+        tipo.classList.add('card-text');
+        tipo.innerHTML = '<strong>Tipo:</strong> ' + item.tipo;
+        var dano = document.createElement('p');
+        dano.classList.add('card-text');
+        dano.innerHTML = '<strong>Dano:</strong> ' + item.dano;
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(tipo);
+        if (item.dano) {
+            cardBody.appendChild(dano);
+        }
+        card.appendChild(cardBody);
+        container.appendChild(card);
+    });
+}
+
+// Atualizar as subcategorias quando uma categoria for selecionada
+// document.getElementById('categoria').addEventListener('change', atualizarSubcategorias);
+var categoriaSelect = document.getElementById('categoria');
+if (categoriaSelect) {
+    console.log('ola');
+    categoriaSelect.addEventListener('change', atualizarSubcategorias);
+}
+
+// Sua lista de equipamentos (exemplo)
+var equipamentos = [
+    {
+        "nome": "item1",
+        "tipo": "espada",
+        "dano": "1d6 + 2",
+        "categoria": "Armas",
+        "subcategoria": "Espada"
+    },
+    {
+        "nome": "item2",
+        "tipo": "Machado",
+        "dano": "1d6 + 2",
+        "categoria": "Armas",
+        "subcategoria": "Machado"
+    },
+    {
+        "nome": "item3",
+        "tipo": "armadura leve",
+        "defesa": "+2",
+        "categoria": "Armaduras",
+        "subcategoria": "Leve"
+    },
+    {
+        "nome": "item4",
+        "tipo": "armadura pesada",
+        "defesa": "+2",
+        "categoria": "Armaduras",
+        "subcategoria": "Pesada"
+    },
+    {
+        "nome": "item5",
+        "tipo": "escudo pequeno",
+        "defesa": "+1",
+        "categoria": "Escudos",
+        "subcategoria": "Pequeno"
+    },
+    {
+        "nome": "item6",
+        "tipo": "escudo medio",
+        "defesa": "+1",
+        "categoria": "Escudos",
+        "subcategoria": "Médio"
+    },
+    {
+        "nome": "item7",
+        "tipo": "item comida",
+        "descricao": "Alguma descrição",
+        "categoria": "Itens",
+        "subcategoria": "Comida"
+    },
+    {
+        "nome": "item8",
+        "tipo": "item geral",
+        "descricao": "Alguma descrição",
+        "categoria": "Itens",
+        "subcategoria": "Geral"
+    },
+    {
+        "nome": "item9",
+        "tipo": "pocao de cura",
+        "efeito": "Cura +20",
+        "categoria": "Poções",
+        "subcategoria": "Vida"
+    },
+    {
+        "nome": "item10",
+        "tipo": "pocao de mana",
+        "efeito": "Cura +20",
+        "categoria": "Poções",
+        "subcategoria": "Mana"
+    }
+];
