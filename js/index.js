375b582155b4c132e18e169180d0edaadccb48e4
David Oliveira <dol.jpo@hotmail.com>
2024-02-20 00:26:48 -0300

    More items added to the list, item properties updated

--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
+/*
+
+Regra de carga:
+
+Você pode carregar 10 espaços +2 por ponto de Força (ou –1 por ponto de Força negativo). 
+Se ultrapassar esse limite, fica sobrecarregado — sofre penalidade de armadura –5 e seu deslocamento é reduzido pela metade. 
+Você não pode carregar mais do que o dobro do seu limite. 
+
+Assim, um personagem
+com Força 2 pode carregar até 14 espaços sem penalidade, e até 28 espaços ficando sobrecarregado. Ele
+não pode carregar mais de 28 espaços de itens
+
+Tipos de itens:
+
+Armas
+    Simples
+    Marcias
+    Exoticas
+
+Armaduras
+    Leves
+    Medias
+    Pesadas
+
+Escudos
+    Pequenos
+    Medios
+    Grandes
+    De Corpo
+
+Itens
+    Munição
+    Diversos
+    Comestivel
+
+Pocoes
+    Cura
+    Mana
+    Diversa
+*/
+
+
 function filtrarItens() {
     var categoriaSelecionada = document.getElementById('categoria').value;
     var subcategoriaSelecionada = document.getElementById('subcategoria').value;
